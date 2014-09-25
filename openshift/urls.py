f219c083031274acd98358a402ce969de1e9085a
Fabio Alessandro Locati <fabiolocati@gmail.com>
2014-09-25 11:33:44 +0200

    Add some modifications

--- openshift/urls.py
+++ openshift/urls.py
@@ -7,4 +7,10 @@ urlpatterns = patterns('',
     # Examples:
 #    url(r'^$', 'openshift.views.home', name='home'),
     url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^login$', 'django.contrib.auth.views.login'),
+    url(r'^logout$', 'canvas.views.logout.user_logout'),
+    url(r'^canvas/', include('canvas.urls')),
+    url(r'^$', 'canvas.views.canvas.list'),
+
 )
